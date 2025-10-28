f471e660972ffd633c8c02456387cf4d236ad4e7
sanjana kumari <sanjana1209kumari@gmail.com>
2025-10-28 22:37:05 +0530

    updates

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ def multiply(a,b):
     """return the product of 2 numbers"""
     return a*b
 result= multiply(4,5)
-print(f"deployment succesfull the resultis: {result})
+print(f"deployment succesfull the resultis: {result}")
       
